08d0d07aa4accde552f1413a6799a76f3b6f4464
Rohan Carty <rohan253@hotmail.com>
2020-05-21 01:58:20 +1000

    Fixed issue with misnamed .so file in the LinuxBuild script

--- LinuxBuild.sh
+++ LinuxBuild.sh
@@ -1,4 +1,5 @@
 cd godot-cpp
 scons platform=linux generate_bindings=yes -j$(nproc)
 cd ..
-scons platform=linux -j$(nproc)
+scons platform=x11 -j$(nproc)
+cp game/bin/x11/liborbitalsimulation.so game/bin/x11/orbitalsimulation.so
