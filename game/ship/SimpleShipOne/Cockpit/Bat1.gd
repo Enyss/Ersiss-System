41037d94b3fbd3b1758689276729dae9637debc7
Enyss <florian.dutheil@gmail.com>
2020-06-07 07:58:13 +0200

    Added Missing Script

---
+++ game/ship/SimpleShipOne/Cockpit/Bat1.gd
@@ -0,0 +1,11 @@
+extends Switch
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	update_state("on")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
