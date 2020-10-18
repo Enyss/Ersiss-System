1da9e4a2fa6fcb153e0a4d8616b440ee3751e333
Justin Abbott <catch.22.designs@gmail.com>
2020-10-18 00:45:07 -0700

    Fixes most invalid path errors and makes player runnable again.
    With this commit, all of the build errors are gone and all of the run errors except those relating to the location of Viewport texture nodes and input calls. The game is now runnable, but not very controllable.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Ersiss-System")]
+// [assembly: AssemblyTitle("Ersiss-System")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("")]
+// [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -16,7 +16,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("1.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
